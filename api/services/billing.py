ddbc6edd7f3a4435d46002aa14d5563980d54f2f
mentesnot-2 <sibhatmentesnot@gmail.com>
2026-03-30 21:47:48 +0300

    added billing service

---
+++ api/services/billing.py
@@ -0,0 +1,34 @@
+import stripe
+from config.settings import STRIPE_SECRET_KEY, STRIPE_PRICE_PRO_MONTHLY, BILLING_SUCCESS_URL, BILLING_CANCEL_URL
+
+
+stripe.api_key = STRIPE_SECRET_KEY
+
+def create_checkout_session(customer_id:str, user_id:int) -> str:
+    try:
+        session = stripe.checkout.Session.create(
+            mode="subscription",
+            customer=customer_id,
+            line_items = [{"price":STRIPE_PRICE_PRO_MONTHLY,"quantity":1}],
+            success_url=BILLING_SUCCESS_URL,
+            cancel_url=BILLING_CANCEL_URL,
+            metadata={"user_id":str(user_id)},
+        )
+
+        return session.url
+    except Exception as e:
+        print(f"Error creating checkout session: {e}")
+        return None
+
+def create_portal_session(customer_id:str) -> str:
+    try:
+        session = stripe.billing_portal.Session.create(
+            customer=customer_id,
+            return_url="http://localhost:3000/dashboard/billing",
+        )
+        return session.url
+    except Exception as e:
+        print(f"Error creating portal session: {e}")
+        return None
+
+
