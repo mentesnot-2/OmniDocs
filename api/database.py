8f7395374c62fcc6f9c56042ce172df27891b9fb
mentesnot-2 <sibhatmentesnot@gmail.com>
2026-04-07 14:04:50 +0300

    changed the database from sqlite to postgress

--- api/database.py
+++ api/database.py
@@ -5,18 +5,11 @@ Database configuration and session management.
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 from pathlib import Path
+import os
 
-# SQLite DB path
-DB_DIR = Path(__file__).resolve().parent.parent / "data"
-DB_DIR.mkdir(parents=True, exist_ok=True)
-DATABASE_URL = f"sqlite:///{DB_DIR}/omnidocs.db"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    # echo=True,
-)
-
+# Replace SQLite with PostgreSQL
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL,pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
