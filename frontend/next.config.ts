6d8a963b4406a14cfd68c1bfb085a55b9a26cea8
mentesnot-2 <sibhatmentesnot@gmail.com>
2026-05-13 10:34:49 +0300

    fixed devindicator logo in next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [{ source: "/api/:path*", destination: `${apiUrl}/:path*` }];
   },
+  devIndicators: false,
 };
 
 export default nextConfig;
